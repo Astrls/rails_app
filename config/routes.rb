7d92c0b91103bb9ce1abbbb064ee677aa46d1deb
Antoine Struelens <antoine.struelens@gmail.com>
2023-09-26 15:44:02 +0200

    logout setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index", as: :about
   get "signup", to: "registration#new"
-  # post "signup", to: "registration#new", as: :signup
+  post "signup", to: "registration#create"
+  delete "logout", to: "sessions#destroy"
 
   # Defines the root path route ("/")
   root to: "main#index" 
